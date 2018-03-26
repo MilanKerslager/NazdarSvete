5462450647e0843d7adfac5299221528704f6877
milan.kerslager <milan.kerslager@W10418.sps-cl.local>
2018-03-26 13:21:36 +0200

    První commit

---
+++ src/nazdarsvěte/NazdarSvěte.java
@@ -0,0 +1,11 @@
+package nazdarsvěte;
+
+public class NazdarSvěte {
+
+    public static void main(String[] args) {
+        System.out.println("Nazdar Světe!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
