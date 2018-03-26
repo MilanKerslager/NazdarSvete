71b031da0e50dc75bfe3151c2af269bb51f67bed
milan.kerslager <milan.kerslager@W10418.sps-cl.local>
2018-03-26 13:35:21 +0200

    čísla vedle sebe

--- src/nazdarsvěte/NazdarSvěte.java
+++ src/nazdarsvěte/NazdarSvěte.java
@@ -5,7 +5,7 @@ public class NazdarSvěte {
     public static void main(String[] args) {
         System.out.println("Nazdar Světe!");
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
     }
 }
